de9aa091e8e870a087efec80fa75e7a3e0054292
Chengqi Lu <43133404+luchengqi7@users.noreply.github.com>
2021-11-15 10:49:14 +0100

    Create ScoreHandler.java

---
+++ src/main/java/org/matsim/analysis/ScoreHandler.java
@@ -0,0 +1,11 @@
+package org.matsim.analysis;
+
+import org.matsim.core.controler.events.IterationEndsEvent;
+import org.matsim.core.controler.listener.IterationEndsListener;
+
+public class ScoreHandler implements IterationEndsListener {
+    @Override
+    public void notifyIterationEnds(IterationEndsEvent iterationEndsEvent) {
+
+    }
+}
